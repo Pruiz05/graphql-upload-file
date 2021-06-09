bf097b78bb754115aecb8f73062e142b7843262c
PedroLuis <pedroluis0542@gmail.com>
2021-06-08 23:38:41 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const { ApolloServer, gql } = require("apollo-server");
+const { createWriteStream } = require("fs");
+const typeDefs = gql`
+  type File {
+    filename: String!
+    mimetype: String!
+    encoding: String!
+  }
+  type Query {
+    uploads: [File]
+  }
+  type Mutation {
+    uploadFile(file: Upload!): File!
+  }
+`;
+
+const resolvers = {
+  Mutation: {
+    async uploadFile(parent, { file }) {
+      const { createReadStream, filename, mimetype, encoding } = await file;
+      const stream = createReadStream();
+      await new Promise((resolve, reject) => {
+        stream
+          .on("error", (error) => {
+            unlink(path, () => {
+              reject(error);
+            });
+          })
+          .pipe(createWriteStream(filename))
+          .on("error", reject)
+          .on("finish", resolve);
+      });
+      console.log("-----------file written");
+      return file;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
