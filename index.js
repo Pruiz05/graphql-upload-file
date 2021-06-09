07f25b303f162880413d62b013e9c3c978a2992d
Pedro Luis <pedroluis0542@gmail.com>
2021-06-08 23:45:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const resolvers = {
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
-// The `listen` method launches a web server.
+
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
